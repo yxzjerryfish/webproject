7eb7d892d526839e8fd28afa072847b765b673ea
Fish Paradise <yxzjerryfish@hotmail.com>
2020-01-15 20:53:50 +0800

    router

--- web-demo/src/main.js
+++ web-demo/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
+import router from '@/router/router.js'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
