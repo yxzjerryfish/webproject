50ce9c712350719e7c67321026d637934b4ee101
Fish Paradise <yxzjerryfish@hotmail.com>
2020-01-21 13:47:32 +0800

    less

--- fish-design-vue/src/App.vue
+++ fish-design-vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <a-button></a-button>
+    <a-button>按钮</a-button>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
