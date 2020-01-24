83a21dd61af69bebb07e1d74afcea6c5bd3f4f49
Fish Paradise <yxzjerryfish@hotmail.com>
2020-01-24 14:00:55 +0800

    布局

--- fish-design-vue/src/layouts/BasicLayout.vue
+++ fish-design-vue/src/layouts/BasicLayout.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    基本布局
+  <div id="components-layout-demo-basic">
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout>
+        <a-layout-sider>Sider</a-layout-sider>
+        <a-layout-content>Content</a-layout-content>
+      </a-layout>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
   </div>
 </template>
 
@@ -8,4 +15,33 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+#components-layout-demo-basic {
+  text-align: center;
+}
+#components-layout-demo-basic .ant-layout-header,
+#components-layout-demo-basic .ant-layout-footer {
+  background: #7dbcea;
+  color: #fff;
+}
+#components-layout-demo-basic .ant-layout-footer {
+  line-height: 1.5;
+}
+#components-layout-demo-basic .ant-layout-sider {
+  background: #3ba0e9;
+  color: #fff;
+  line-height: 120px;
+}
+#components-layout-demo-basic .ant-layout-content {
+  background: rgba(16, 142, 233, 1);
+  color: #fff;
+  min-height: 120px;
+  line-height: 120px;
+}
+#components-layout-demo-basic > .ant-layout {
+  margin-bottom: 48px;
+}
+#components-layout-demo-basic > .ant-layout:last-child {
+  margin: 0;
+}
+</style>
