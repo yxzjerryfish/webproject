53a7f4cea5dfdcba24e443b959cb2173ef3b1e86
Fish Paradise <yxzjerryfish@hotmail.com>
2020-01-25 23:11:23 +0800

    修正了一下格式

--- fish-design-vue/src/layouts/Header.vue
+++ fish-design-vue/src/layouts/Header.vue
@@ -4,6 +4,24 @@
       <a-avatar shape="square" :size="64" icon="user" />
     </div>
     my-header
+    <div class="headdropdown">
+      <a-dropdown>
+        <a class="ant-dropdown-link" href="#">
+          Hover me <a-icon type="down" />
+        </a>
+        <a-menu slot="overlay">
+          <a-menu-item>
+            <a href="javascript:;">1st menu item</a>
+          </a-menu-item>
+          <a-menu-item>
+            <a href="javascript:;">2nd menu item</a>
+          </a-menu-item>
+          <a-menu-item>
+            <a href="javascript:;">3rd menu item</a>
+          </a-menu-item>
+        </a-menu>
+      </a-dropdown>
+    </div>
   </div>
 </template>
 
@@ -14,5 +32,8 @@ export default {};
 <style>
 .headavatar {
   float: left;
+}
+.headdropdown {
+  float: right;
 }
 </style>
